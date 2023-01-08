cecda43ce551deb7487334bf99f119d4769cd36e
shouyinji1 <shouyinji1@DESKTOP-HPB7UDD.lan>
2023-01-08 19:41:51 +0800

    添加：chmod添加tor、snowflake的执行权限

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 # Tor文件夹复制自Tor Browser(tor-browser/Browser/TorBrowser/Tor/)
 
 #cd /home/shouyinji1/Desktop/GIT_Repo/snowflake/client/
+chmod u+x ./Tor/tor ./Tor/PluggableTransports/snowflake-client
 ./Tor/tor -f ./my-torrc
